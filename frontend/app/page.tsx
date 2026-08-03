8b2146fb76ed5bb34dba2d751b3ee5ccad169354
diabalsadi <alsadidiab@gmail.com>
2026-08-03 23:30:43 +0300

    wip

---
+++ frontend/app/page.tsx
@@ -0,0 +1,3 @@
+import HomePage from "./(user)/page";
+
+export default HomePage;
--- frontend/components/RichEditor/index.tsx
+++ frontend/components/RichEditor/index.tsx
@@ -6,7 +6,7 @@ import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
 import Youtube from "@tiptap/extension-youtube";
 import TextAlign from "@tiptap/extension-text-align";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 import Highlight from "@tiptap/extension-highlight";
 import styles from "./RichEditor.module.scss";
